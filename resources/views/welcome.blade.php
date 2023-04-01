f88a214f8dea1358553e6317845af4e377dc8310
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-02 00:33:35 +0600

    api integrated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,8 +36,9 @@
                     <ul class="m-b-0">
                         <li class="clearfix">
                             <div class="message-data text-right">
-                                
+
                                 <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
+                                
                             </div>
 
                             @if(isset($output))
