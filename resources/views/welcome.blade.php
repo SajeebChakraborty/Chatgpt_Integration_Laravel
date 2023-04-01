5d2836e8e86158b3914ea630ab2d395d3b35f2c8
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-02 00:33:14 +0600

    api integrated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,7 @@
                     <ul class="m-b-0">
                         <li class="clearfix">
                             <div class="message-data text-right">
+                                
                                 <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
                             </div>
 
